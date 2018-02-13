f635b4b0a802193fab188d7271675ecb90d941d5
dortonway <dortonway@gmail.com>
2018-02-13 21:25:54 +0300

    totrello with lots of fields

--- src/totrello/index.js
+++ src/totrello/index.js
@@ -1,26 +1,31 @@
 #!/usr/bin/env node
-const { exec } = require('child_process');
+const request = require('request');
+const parse = require('csv-parse/lib/sync');
 const stdin = process.openStdin();
 
-stdin.on('data', task => {
-  task = String(task).replace('\n', '');
-
-
-  var request = require("request");
-
-  var options = { method: 'POST',
+stdin.on('data', input => {
+  input = String(input).replace('\n', '');
+  // console.log({input});
+  
+  const [ idList, name, desc, idMembers, due ] = parse(input)[0];
+  
+  const options = {
+    method: 'POST',
     url: 'https://api.trello.com/1/cards',
-    qs: 
-     { name: task,
-       idList: '5a820d4e3ec5319990007898',
-       keepFromSource: 'all',
-       key: '09ee14411f546915a65d690b1a8d36b0',
-       token: 'cb3a3b3c080047af57ad3d4f37ac45e252aecf11ba5e577d6ff01072d899954e' } };
+    qs: {
+      idList,
+      name,
+      desc,
+      idMembers,
+      due,
+      keepFromSource: 'all',
+      key: '09ee14411f546915a65d690b1a8d36b0',
+      token: 'cb3a3b3c080047af57ad3d4f37ac45e252aecf11ba5e577d6ff01072d899954e',
+    },
+  };
 
-  request(options, function (error, response, body) {
+  request(options, (error, response, body) => {
     if (error) throw new Error(error);
-
     console.log(body);
   });
 });
-
