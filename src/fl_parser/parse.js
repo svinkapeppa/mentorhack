ed9a05fd6dc5cc88dca4b25c2ed1d4cfe4b9c748
dortonway <dortonway@gmail.com>
2018-02-13 20:43:11 +0300

    parser to csv

--- src/fl_parser/parse.js
+++ src/fl_parser/parse.js
@@ -3,11 +3,26 @@ const jsdom = require('jsdom');
 const csvStringify = require('csv-stringify');
 const { JSDOM } = jsdom;
 
-// let pages = fs.readdirSync('./pages').filter(el => el !== '.' && el !== '..');
-//
-// const tasks = pages.map(page => {
-//   const body = fs.readFileSync('./pages/' + page);
-  const body = fs.readFileSync('./pages/1056095.html');
+const n = process.argv[2];
+if (!n) throw new Error('pass n');
+
+const pagesPath = './pages_' + n;
+let i = 0;
+
+console.log({n});
+
+// const stringify = data => new Promise((resolve, reject) => {
+//   return csvStringify(data, (err, csv) => {
+//     if (err) return console.log(`Error: ${err}`);
+//     return resolve(csv);
+//   });
+// });
+
+let pages = fs.readdirSync(pagesPath).filter(el => el !== '.' && el !== '..');
+
+const tasks = pages.map(page => {
+  const body = fs.readFileSync(`${pagesPath}/${page}`);
+  // const body = fs.readFileSync('./pages/1056093.html');
   const dom = new JSDOM(body);
   const document = dom.window.document;
 
@@ -19,10 +34,15 @@ const { JSDOM } = jsdom;
     '.b-layout__txt.b-layout__txt_padbot_20'
   ).textContent.trim();
 
-  csvStringify([[taskTitle, taskBody]], (err, csv) => {
-    if (err) return console.log(`Error: ${err}`);
-    console.log(csv);
-  });
+  console.log(++i);
 
-  // return { taskTitle, taskBody };
-// });
+  return taskTitle && taskBody ? [ taskTitle, taskBody ] : null;
+});
+
+const completedTasks = tasks.filter(t => t !== null);
+
+csvStringify(completedTasks, (err, csv) => {
+  if (err) return console.error(`Error: ${err}`);
+  // console.log({csv, tasks});
+  fs.writeFileSync(`./out${n}.csv`, csv);
+});
