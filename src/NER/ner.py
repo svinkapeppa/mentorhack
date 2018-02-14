18e6a21ca3431ccd037bc2cc8b8de75eac84687b
Marat Zaynutdinov <tsundokum@gmail.com>
2018-02-14 15:46:56 +0300

    ner works a bit

--- src/NER/ner.py
+++ src/NER/ner.py
@@ -1,17 +1,27 @@
 import sys
 import re
-from natasha import NamesExtractor, DatesExtractor, MoneyExtractor, AddressExtractor, OrganisationExtractor 
+from natasha import NamesExtractor, DatesExtractor, MoneyExtractor, AddressExtractor, OrganisationExtractor
+from natasha.extractors import Extractor
 from pymystem3 import Mystem
-from datetime import datetime
+from datetime import datetime, timedelta
+
+# from date_grammar import DATE
 
 dates_extractor = DatesExtractor() 
 name_extractor = NamesExtractor() 
 money_extractor = MoneyExtractor()
 organization_extractor = OrganisationExtractor() 
 address_extractor = AddressExtractor()
-m = Mystem()
 
 m = Mystem()
+
+def lemmatize(s):
+    global m
+    try:
+        return ''.join(m.lemmatize(s)).strip()
+    except BrokenPipeError as ex:
+        m = Mystem()
+        return lemmatize(s)
 
 #for line in sys.stdin:
 #    matches = dates_extractor(line)
@@ -20,6 +30,7 @@ m = Mystem()
 #        facts.append(match.fact)
 #    print(facts)
 
+
 def extract_users(text):
     matches = re.search('@\w+', text)
     res = []
@@ -27,37 +38,49 @@ def extract_users(text):
         res.append(m)
     return res
 
+
 def extract_date(text):
-    text = m.lemmatize(text)
-    matches = re.search('@\d+ день|неделя|месяц|год', text)
+    text = lemmatize(text)
+    matches = re.search('\d+ день|неделя|месяц|год', text)
 
     if 'завтра' in text:
-        return datetime.now() + datetime.timedelta(days=1)
+        return datetime.now() + timedelta(days=1)
     elif 'послезавтра' in text:
-        return datetime.now() + datetime.timedelta(days=2) 
+        return datetime.now() + timedelta(days=2)
     elif text == 'один неделя':
-        return datetime.now() + datetime.timedelta(weeks=1) 
+        return datetime.now() + timedelta(weeks=1)
     elif text == 'два неделя':
-        return datetime.now() + datetime.timedelta(weeks=2)       
+        return datetime.now() + timedelta(weeks=2)
     elif text == 'один день':
-        return datetime.now() + datetime.timedelta(days=1)     
+        return datetime.now() + timedelta(days=1)
     elif text == 'два день':
-        return datetime.now() + datetime.timedelta(days=2)     
+        return datetime.now() + timedelta(days=2)
     elif text == 'три день':
-        return datetime.now() + datetime.timedelta(days=3)                     
+        return datetime.now() + timedelta(days=3)
     elif text == 'один месяц':
-        return datetime.now() + datetime.timedelta(month=1)                         
+        return datetime.now() + timedelta(month=1)
     elif len(matches) > 0: #regex check
-        return ???
+        return  # TODO
     elif text == 'день':
-        return datetime.now() + datetime.timedelta(day=1)                         
+        return datetime.now() + timedelta(day=1)
     elif text == 'неделя': 
-        return datetime.now() + datetime.timedelta(week=1)                         
+        return datetime.now() + timedelta(week=1)
     elif text == 'месяц':
-        return datetime.now() + datetime.timedelta(month=1)                         
+        return datetime.now() + timedelta(month=1)
     else: #natasha
         pass
 
     facts = []
     for match in matches:
         return match.fact
+
+
+# class MyDatesExtractor(Extractor):
+#     def __init__(self):
+#         super(MyDatesExtractor, self).__init__(DATE)
+# de = MyDatesExtractor()
+# res = de('к 1 февраля')
+# print(len(res))
+# print(res[0])
+
+print(extract_date('завтра'))
