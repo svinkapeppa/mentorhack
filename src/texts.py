3fd72cdc031300e5e571783f2c88aa51a7268ecc
Marat Zaynutdinov <tsundokum@gmail.com>
2018-02-14 17:21:42 +0300

    rm old ner file

--- src/texts.py
+++ src/texts.py
@@ -1,4 +1,4 @@
-trello_list_needed_text = '''необходимо вас зарегестрировать! Для этого:
+trello_list_needed_text = '''необходимо вас зарегистрировать! Для этого:
         - перейдите по [ссылке](https://trello.com/1/authorize?expiration=never&name=SinglePurposeToken&key=b0e80e8c1862ec65407d67bfc4a87fd4&response_type=token)
         - нажмите "ALLOW"
         - запомните токен
