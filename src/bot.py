96b07d4e2d5bc9dffe60385226f8a0b6ee361f4b
bavadim@gmail.com <bavadim@gmail.com>
2018-02-15 00:48:29 +0300

    start batching

--- src/bot.py
+++ src/bot.py
@@ -32,7 +32,7 @@ def write_task(token, list_id, summary, text, assignees, due_date, chat_id):
     to_exec = "echo '" + json.dumps(payload) + "' | " + os.getcwd() + "/totrello/index.js"
     response = json.loads(os.popen(to_exec).read())
 
-    print(response, file=sys.stderr)
+    #print(response, file=sys.stderr)
     url = response['newCard']['shortUrl']
     write_to_telegram([chat_id, url, '', '', '', ''])
 
@@ -67,48 +67,51 @@ def gen_summary(text):
 def format_body(text):
     return text
 
+def process_message(chat_id, from_, from_mention, question):
+    from_mention = '@' + from_mention
+
+    list_id = chat_id2list_id(chat_id)
+    token = chat_id2token(chat_id)
+    
+    if chat_id == from_ and from_mention not in TELEGRAM2TRELLO and is_email(question):
+        TELEGRAM2TRELLO[from_mention] = question
+        write_to_telegram([chat_id, snx, '', '','',''])
+
+    if (list_id is None or token is None) and is_trello_token(question):
+        CHAT2TOKENS[chat_id] = question.split(' ')[0]
+        CHAT2LISTS[chat_id] = question.split(' ')[1]
+        write_to_telegram([chat_id, trello_list_added_text, '', '','',''])
+        continue
+
+    if list_id is None or token is None:
+        write_to_telegram([chat_id, trello_list_needed_text, '', '','',''])
+        continue
+        
+    if not is_task(question) and KAZEMIR_MENTION not in question:
+        continue
+
+    summary = gen_summary(question)
+    text = format_body(question)
+    assignees = []
+    unknown_telegram_users = []
+    for user in extract_users(question):
+        if user in TELEGRAM2TRELLO:
+            if user != KAZEMIR_MENTION:
+                assignees.append(TELEGRAM2TRELLO[user])
+        else:
+            unknown_telegram_users.append(user)
+
+    if len(unknown_telegram_users) > 0:
+        write_to_telegram([chat_id, user_email_needed.format(','.join(unknown_telegram_users)), '', '','',''])
+        
+    due_date = extract_date(question)
+
+    write_task(token, list_id, summary, text, assignees, due_date, chat_id)
+
 def main():
     for row in csv.reader(iter(sys.stdin.readline, '')):
         chat_id, from_, from_mention, question = row[0], row[1], row[2], row[3]
-        from_mention = '@' + from_mention
-
-        list_id = chat_id2list_id(chat_id)
-        token = chat_id2token(chat_id)
-        
-        if chat_id == from_ and from_mention not in TELEGRAM2TRELLO and is_email(question):
-            TELEGRAM2TRELLO[from_mention] = question
-            write_to_telegram([chat_id, snx, '', '','',''])
-
-        if (list_id is None or token is None) and is_trello_token(question):
-            CHAT2TOKENS[chat_id] = question.split(' ')[0]
-            CHAT2LISTS[chat_id] = question.split(' ')[1]
-            write_to_telegram([chat_id, trello_list_added_text, '', '','',''])
-            continue
-
-        if list_id is None or token is None:
-            write_to_telegram([chat_id, trello_list_needed_text, '', '','',''])
-            continue
-            
-        if not is_task(question) and KAZEMIR_MENTION not in question:
-            continue
-
-        summary = gen_summary(question)
-        text = format_body(question)
-        assignees = []
-        unknown_telegram_users = []
-        for user in extract_users(question):
-            if user in TELEGRAM2TRELLO:
-                if user != KAZEMIR_MENTION:
-                    assignees.append(TELEGRAM2TRELLO[user])
-            else:
-                unknown_telegram_users.append(user)
-
-        if len(unknown_telegram_users) > 0:
-            write_to_telegram([chat_id, user_email_needed.format(','.join(unknown_telegram_users)), '', '','',''])
-            
-        due_date = extract_date(question)
-
-        write_task(token, list_id, summary, text, assignees, due_date, chat_id)
+        process_message(chat_id, from_, from_mention, question)
 
 if __name__ == "__main__":
     main()
