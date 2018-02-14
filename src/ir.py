ec3e2ed84a4ef1e97c044cb8d59e2fcdd344bb3d
Vadim Polulyakh <bavadim@gmail.com>
2018-02-14 17:06:26 +0300

    adding trello integration

--- src/ir.py
+++ src/ir.py
@@ -1,6 +1,7 @@
 import pickle
 from pathlib import Path
 from nltk.tokenize import wordpunct_tokenize
+import re
 
 MODELS = Path('../models/ir').expanduser()
 
@@ -11,3 +12,10 @@ def is_task(s):
     lines = [' '.join(wordpunct_tokenize(line.strip())) for line in s.splitlines() if line.strip()]
     text = '\n'.join(lines)
     return classifier.predict([text])[0]
+
+def is_trello_token(s):
+    if re.match('^[\w\d]{64} [\w\d]{24}$', s) is not None:
+        return True
+    else:
+        return False
+    
--- src/ner.py
+++ src/ner.py
@@ -21,43 +21,44 @@ m = Mystem()
 #    print(facts)
 
 def extract_users(text):
-    matches = re.search('@\w+', text)
+    p = re.compile('@\w+')
     res = []
-    for m in matches:
-        res.append(m)
+    for m in p.finditer(text):
+        res.append(m.group())
     return res
 
 def extract_date(text):
     text = m.lemmatize(text)
-    matches = re.search('@\d+ день|неделя|месяц|год', text)
+    matches = re.search('\d+ день|неделя|месяц|год', ' '.join(text))
 
-    if 'завтра' in text:
-        return datetime.now() + datetime.timedelta(days=1)
-    elif 'послезавтра' in text:
-        return datetime.now() + datetime.timedelta(days=2) 
-    elif text == 'один неделя':
-        return datetime.now() + datetime.timedelta(weeks=1) 
-    elif text == 'два неделя':
-        return datetime.now() + datetime.timedelta(weeks=2)       
-    elif text == 'один день':
-        return datetime.now() + datetime.timedelta(days=1)     
-    elif text == 'два день':
-        return datetime.now() + datetime.timedelta(days=2)     
-    elif text == 'три день':
-        return datetime.now() + datetime.timedelta(days=3)                     
-    elif text == 'один месяц':
-        return datetime.now() + datetime.timedelta(month=1)                         
-    elif len(matches) > 0: #regex check
-        return ???
-    elif text == 'день':
-        return datetime.now() + datetime.timedelta(day=1)                         
-    elif text == 'неделя': 
-        return datetime.now() + datetime.timedelta(week=1)                         
-    elif text == 'месяц':
-        return datetime.now() + datetime.timedelta(month=1)                         
-    else: #natasha
-        pass
+  #  if 'завтра' in text:
+  #      return datetime.now() + datetime.timedelta(days=1)
+  #  elif 'послезавтра' in text:
+  #      return datetime.now() + datetime.timedelta(days=2) 
+  #  elif text == 'один неделя':
+  #      return datetime.now() + datetime.timedelta(weeks=1) 
+  #  elif text == 'два неделя':
+  #      return datetime.now() + datetime.timedelta(weeks=2)       
+  #  elif text == 'один день':
+  #      return datetime.now() + datetime.timedelta(days=1)     
+  #  elif text == 'два день':
+  #      return datetime.now() + datetime.timedelta(days=2)     
+  #  elif text == 'три день':
+  #      return datetime.now() + datetime.timedelta(days=3)                     
+  #  elif text == 'один месяц':
+  #      return datetime.now() + datetime.timedelta(month=1)                         
+  #  elif len(matches) > 0: #regex check
+  #      return ???
+  #  elif text == 'день':
+  #      return datetime.now() + datetime.timedelta(day=1)                         
+  #  elif text == 'неделя': 
+  #      return datetime.now() + datetime.timedelta(week=1)                         
+  #  elif text == 'месяц':
+  #      return datetime.now() + datetime.timedelta(month=1)                         
+  #  else: #natasha
+  #      pass
 
-    facts = []
-    for match in matches:
-        return match.fact
+ #   facts = []
+ #   for match in matches:
+ #       return match.fact
+    return datetime.now()
