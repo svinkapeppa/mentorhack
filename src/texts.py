f9c8fc28fdbebd7dccaa3429d645df6b3dfcd53d
Vadim Polulyakh <bavadim@gmail.com>
2018-02-14 19:44:28 +0300

    wip

--- src/texts.py
+++ src/texts.py
@@ -1,8 +1,8 @@
 trello_list_needed_text = '''необходимо вас зарегистрировать! Для этого:
-        - перейдите по [ссылке](https://trello.com/1/authorize?expiration=never&name=SinglePurposeToken&key=b0e80e8c1862ec65407d67bfc4a87fd4&response_type=token)
+        - перейдите на https://trello.com/1/authorize?expiration=never&name=SinglePurposeToken&key=b0e80e8c1862ec65407d67bfc4a87fd4&response_type=token
         - нажмите "ALLOW"
         - запомните токен
-        - откройте или создайте доску в [Trello](https://trello.com) и в URL браузера замените имя доски на report.json
+        - откройте или создайте доску в https://trello.com и в URL браузера замените имя доски на report.json
         - вы получите описание вашей доски, найдите объект списка с нужным вам именем (поле name)
         - запомните ид списка
         - пришлите в чат сообщение в формате <токен> <имя списка> 
