41d4602efeedea0965bf4e2ca339e0b605866969
tsundokum <tsundokum@gmail.com>
2018-02-15 04:03:20 +0300

    correctly parse к следующему пн

--- src/NER/ner.py
+++ src/NER/ner.py
@@ -1,7 +1,4 @@
-import sys
 import re
-from natasha import NamesExtractor, DatesExtractor, MoneyExtractor, AddressExtractor, OrganisationExtractor
-from natasha.extractors import Extractor
 from pymystem3 import Mystem
 from datetime import datetime, timedelta
 
@@ -24,10 +21,27 @@ def extract_users(text):
 
 count_names = 'один|два|три|четыре|пять|шесть|семь|восемь|девять|десять'.split('|')
 
+weekdays = {'понедельник':0,'пн':0,
+            'вторник':1,'вт':1,
+            'среда':2,'ср':2,
+            'четверг':3,'чт':3,
+            'пятница':4,'пт':4,
+            'суббота':5,'сб':5,
+            'воскресенье':6,'вс':6}
+
+
+def next_weekday(d, weekday):
+    days_ahead = weekday - d.weekday()
+    if days_ahead <= 0: # Target day already happened this week
+        days_ahead += 7
+    return d + timedelta(days_ahead)
+
+
 def extract_date(orig_text):
     text = lemmatize(orig_text.lower())
     digit_matches = re.search('(\\d+) (день|неделя|месяц|год)', text)
     word_matches = re.search('(один|два|три|четыре|пять|шесть|семь|восемь|девять|десять) (день|неделя|месяц|год)', text)
+    next_matches = re.search('следующий (понедельник|пн|вторник|вт|среда|ср|четверг|чт|пятница|пт|суббота|сб|воскресенье|вс)', text)
 
     if 'завтра' in text:
         return datetime.now() + timedelta(days=1)
@@ -57,6 +71,9 @@ def extract_date(orig_text):
             return datetime.now() + timedelta(days=count*30)
         elif datenames == 'год':
             return datetime.now() + timedelta(days=count*365)
+    elif next_matches:
+        weekday_name = next_matches.group(1)
+        return next_weekday(datetime.now(), weekdays[weekday_name])
 
     # срок тебе неделя
     elif 'день' in text:
@@ -76,6 +93,8 @@ if __name__ == '__main__':
 даю тебе 4 дня на реализацию
 код должен работать через 2 месяца начиная с сейчас    
 срок тебе неделя
+сделай к следующему четвергу
+к следующему пн
 тут нет даты'''
     print('now', datetime.now())
     for line in tests.splitlines():
